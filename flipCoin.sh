36546e9ace1d74548b065b4c5888960ee317162a
karuna <karunakakade8@gmail.com>
2021-04-23 23:43:33 +0530

    Added: Determine the percentage

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,10 +1,28 @@
 #!/bin/bash -x
 
+declare -A headDict
+declare -A tailDict
 coin=$((RANDOM%2))
-
-if [ $coin -eq 1 ]
+h=0
+t=0
+for ((i=0; i<=10; i++))
+do
+if [ $((RANDOM%2)) -eq 0 ]
 then
- echo HEADS
-else
- echo TAILS
+  headDict["head"]="HEAD"
+  h=$h+1
+   Head=$(($h/10*100))
+elif [ $((RANDOM%2)) -eq 1 ]
+then
+  tailDict["tail"]="TAIL"
+  t=$t+1
+   Tail=$(($t/10*100))
 fi
+done
+
+#Head=$(($h/10*100))
+
+#Tail=$(($t/10*100))
+
+echo "Heads: " $Head "%"
+echo "Tails: " $Tail "%"
